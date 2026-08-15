9d16b4c9473992d5daf74b54b77cb4e65844ff69
Christian <meng.chr@googlemail.com>
2026-08-15 16:42:04 +0200

    Geburtstagsseite

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const CONFIG = {
 
   // Geburtstagsgruß (HTML erlaubt, z.B. <br> für Zeilenumbruch)
   message:
-    "Alles Gute zum Geburtstag! Ich hoffe, Mainz 05 schenkt dir dieses Jahr genauso viel Freude wie du deinen Mitmenschen. 🔴⚪",
+    "Alles Gute zum Geburtstag! Ich hoffe, Mainz 05 schenkt dir dieses Jahr genauso viel Freude wie du deinen Mitmenschen - oder zumindest hast du deswegenkeine Nervenzusammenbrüchen :D. Aus diesem Grund hier ein paar hilfreiche Vorhersagen und Infos zu der Saison :-) 🔴⚪",
 
   // Optionale Fotos: Dateien in den Ordner "images/" legen und hier eintragen.
   // Beispiel: ["images/foto1.jpg", "images/foto2.jpg"]
